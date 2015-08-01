cf8bc3f895e258dad56f0e2f3cd77ab6e6349ed3
flyingbee2012 <wubiasu@gmail.com>
2015-07-31 19:57:47 -0700

    treeData moved to external file

--- js/draw-svg.js
+++ js/draw-svg.js
@@ -196,10 +196,16 @@ svg_page.append("foreignObject")
                 .attr("height", viewportheight - 140);
                 //.style("color", "rgba(0,0,0,1)");
 
+var dd;
+console.log("abc");
+
 d3.text("pages/page1.html", function (data) {
+    console.log("HTML Data:" + data);
+    dd = data;
     svg_page.select(".svg-p-text")
             .html("<div class='svg-contents'>" + data + "</div>");
 });
+
 
 //Responsive
 window.onresize = responsiveSVG;
@@ -284,8 +290,7 @@ function openPage(d)
         // clear the chart
         svg.selectAll("g.node").remove();
         svg.selectAll("path.link").remove();
-        //root="";
-        //update(root);
+       
     }
     svg_page.select("rect")
             .transition()
@@ -295,324 +300,10 @@ function openPage(d)
                     .duration(1000)
                     .attr("height", viewportheight - 40);
 
-        
-
-    //alert(d.source);
 }
 
 
 // draw the resume tree
-
-var treeData = [
-  {
-      "name": "Bi Wu",
-      "parent": "null",
-      "children": [
-        {
-            "name": "Work Experience"
-          , "children": [
-                {
-                    "name": "PayPal - Software Engineer"
-              , "_children": [
-                {
-                    "name": "SSO (Single Sign-on)",
-                    "url": "https://www.youtube.com/v/-jm5_PAxtSc"
-                }
-
-              ]
-
-                },
-            {
-                "name": "Intel - UI Developer",
-                "parent": "Architect",
-                "_children": [
-                  {
-                      "name": "Multi-tenant Architecture",
-                      "url": "https://www.youtube.com/v/jrKA3cJmoms"
-                  }
-
-                ]
-            },
-            {
-                "name": "ASU - Research Assistant"
-              , "_children": [
-                {
-                    "name": "Marketing Cloud",
-
-                }
-                , {
-                    "name": "Customer Journey",
-                    "url": "https://www.youtube.com/v/OUWwZcVA-mM"
-                }
-               , {
-                   "name": "Social Studio",
-                   "url": "https://www.youtube.com/v/27P6am1sIFQ"
-               },
-                {
-                    "name": "Best Email Practices",
-                    "url": "https://www.youtube.com/v/UqRGfbPsTAI"
-                }
-                ,
-                {
-                    "name": "Social Best Practices for Events",
-                    "url": "https://www.youtube.com/v/hDjqBEy-a4M"
-                }
-                 ,
-                {
-                    "name": "Deliverability Guide to the Inbox",
-                    "url": "https://www.youtube.com/v/NUEt3cZmSTo"
-                }
-                 ,
-                {
-                    "name": "Achieve List Growth Results",
-                    "url": "https://www.youtube.com/v/64ERoj3wpxw"
-                }
-
-              ]
-
-
-            },
-            {
-                "name": "ASU - Teaching Assistant"
-              , "_children": [
-                {
-                    "name": "Communities",
-                    "url": "https://www.youtube.com/v/iVS0p7xAVLQ"
-                }
-
-              ]
-
-            },
-              {
-                  "name": "Qualia - Web Developer"
-              , "_children": [
-                {
-                    "name": "Communities",
-                    "url": "https://www.youtube.com/v/iVS0p7xAVLQ"
-                }
-
-              ]
-
-              }
-          
-
-          ]
-        },
-        {
-            "name": "Academic Projects"
-          , "_children": [
-            {
-                "name": "3D Visualizer"
-
-              , "_children": [
-               {
-                   "name": "Salesforce Data Access and Security",
-                   "url": "https://www.youtube.com/v/17dr2GMvgd8"
-               },
-
-                {
-                    "name": "Who Sees What",
-                    "url": "https://www.youtube.com/v/X3Hg6OXhPO8"
-                },
-                {
-                    "name": "Introduction to the Salesforce Security Model",
-                    "url": "https://www.youtube.com/v/c1ccSXlVjXk"
-                }
-
-              ]
-
-            },
-            {
-                "name": "Data Visualization"
-               , "_children": [
-                {
-                    "name": "Get Started with Sales Cloud Administration",
-                    "url": "https://www.youtube.com/v/MSp61khlP9s"
-                }
-               ]
-            },
-            {
-                "name": "Ray Tracer"
-               , "_children": [
-                {
-                    "name": "Get Started with Service Cloud Administration",
-                    "url": "https://www.youtube.com/v/fJFlIqtlscI"
-                }
-
-               ]
-            }
-          ]
-
-        },
-        {
-            "name": "Honors and Certificates"
-          , "_children": [
-            {
-                "name": "IBM Certificed ODB solution Designer"
-              , "_children": [
-                {
-                    "name": "Clicks or Code",
-                    "url": "https://www.youtube.com/v/R_Ir4bq4px0"
-                }
-
-              ]
-
-            },
-            {
-                "name": "C++ R2"
-              , "_children": [
-                {
-                    "name": "Apex Coding Webinar",
-                    "url": "https://www.youtube.com/v/WBeCWlbGX38"
-                }
-
-              ]
-
-            },
-
-            {
-                "name": "First Class..."
-               , "_children": [
-                {
-                    "name": "Metadata API Webinar",
-                    "url": "https://www.youtube.com/v/S_R0Tzp1HmY"
-                }
-
-               ]
-            }
-          ]
-
-        },
-
-       {
-           "name": "Education"
-              , "_children": [
-                {
-                    "name": "ASU",
-                    "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                },
-                {
-                    "name": "UESTC",
-                    "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                }
-
-              ]
-
-       },
-  {
-      "name": "Skills"
-              , "_children": [
-                 {
-                     "name": "Programming",
-                     "_children": [
-                         {
-                             "name": "C/C++",
-                             "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                         },
-                         {
-                             "name": "Java",
-                             "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                         },
-                         {
-                             "name": "C#",
-                             "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                         },
-                         {
-                             "name": "Python",
-                             "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                         },
-                         {
-                             "name": "Assembly Language",
-                             "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                         },
-                         {
-                             "name": "Win32 Programming",
-                             "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                         },
-                         {
-                             "name": "MFC",
-                             "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                         }
-
-                     ]
-
-                 }
-                , {
-                    "name": "Database",
-                    "_children": [
-                        {
-                            "name": "MS SQL Server",
-                            "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                        },
-                        {
-                            "name": "MySQL",
-                            "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                        },
-                        {
-                            "name": "MS Access",
-                            "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                        }
-                    ]
-                }
-               , {
-                   "name": "Web Development",
-                   "_children": [
-                     {
-                         "name": "ASP.NET",
-                         "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                     },
-                     {
-                         "name": "Django",
-                         "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                     },
-                     {
-                         "name": "JavaScript / jQuery",
-                         "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                     },
-                     {
-                         "name": "HTML / CSS",
-                         "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                     }
-                   ]
-               }
-                ,
-                {
-                    "name": "Computer Graphics",
-                    "_children": [
-                     {
-                         "name": "OpenGL",
-                         "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                     },
-                     {
-                         "name": "Qt",
-                         "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                     },
-                     {
-                         "name": "D3",
-                         "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                     },
-                     {
-                         "name": "DirectX",
-                         "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                     },
-                     {
-                         "name": "OSG",
-                         "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                     },
-                     {
-                         "name": "GLSL",
-                         "url": "https://www.youtube.com/v/N5XNIuVdBf0"
-                     }
-
-                    ]
-                }
-              ]
-
-  }
-
-      ]
-  }
-];
 
 
 //These functions are to draw charts
@@ -641,7 +332,7 @@ function drawForceChart(dataSource) {
     svg_page.select(".svg-p-text")
             .attr("height", "1");
 
-    root = treeData[0];
+    root = resumeData[0];
     root.x0 = height / 2;
     root.y0 = 0;
 
